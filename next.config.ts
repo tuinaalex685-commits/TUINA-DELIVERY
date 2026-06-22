9ad5608c52f3d02c20ddf3dc110f3df18217314c
tuinaalex685-commits <tuinaalex685@users.noreply.github.com>
2026-06-22 01:13:41 +0000

    feat: transformation en PWA (T Delivery)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+});
 
-export default nextConfig;
+const nextConfig: NextConfig = {};
+
+export default withPWA(nextConfig);
